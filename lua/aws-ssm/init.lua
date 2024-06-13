a8fe351e3d3203e2ee7ac0841aa44bc8ea53d5bc
elgormus <elgormus@freelancer.run>
2024-06-14 00:59:57 +0100

    init

--- lua/aws-ssm/init.lua
+++ lua/aws-ssm/init.lua
@@ -2,7 +2,7 @@
 
 local M = {}
 
-function M.save_to_ssm(text, path, profile)
+local function save_to_ssm(text, path, profile)
   local cmd = string.format(
     "aws ssm put-parameter --name '%s' --value '%s' --type SecureString --profile %s > /dev/null 2>&1 ; echo $?",
     path, text, profile
@@ -19,4 +19,20 @@ function M.save_to_ssm(text, path, profile)
   end
 end
 
+function M.ssm()
+  local text = vim.fn.input("Enter Text: ")
+  local path = vim.fn.input("Enter Path: ")
+  local profile = vim.fn.input("Enter Profile: ")
+
+  local clipboard_content = vim.fn.getreg('*')
+  if clipboard_content ~= '' then
+    text = clipboard_content
+  end
+  
+  print("")
+  
+  save_to_ssm(text, path, profile)
+end
+
 return M
+
