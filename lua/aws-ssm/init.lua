f6a162169a7a7535da8f205785aa584f4b588192
oguzhan <svartonn@gmail.com>
2024-06-18 20:52:53 +0100

    refactor ssm modules

--- lua/aws-ssm/init.lua
+++ lua/aws-ssm/init.lua
@@ -1,99 +1,19 @@
--- Author: Oguzhan Coskun
--- Mail: svartonn@gmail.com
+-- init.lua
+local ssm = require('ssm')
 
 local M = {}
 
-local use_fidget = false
-
 function M.setup(options)
-  if options and options.fidget then
-    use_fidget = true
-  end
-end
+  ssm.setup(options)
 
-local function send_notification(message, level)
-  if use_fidget then
-    local fidget = require('fidget')
-    if fidget and fidget.notify then
-      fidget.notify(message, level)
-    else
-      vim.notify(message, level)
-    end
-  else
-    vim.notify(message, level)
-  end
-end
-
-
-local function check_parameter(path, profile)
-  local cmd = string.format(
-    "aws ssm get-parameter --name '%s' --profile %s > /dev/null 2>&1 ; echo $?",
-    path, profile
+  -- Register the custom command
+  vim.api.nvim_create_user_command(
+    'PSMPut',
+    function()
+      ssm.ssm()
+    end,
+    { nargs = 0 }
   )
-
-  local handle = io.popen(cmd)
-  local exit_status = handle:read("*n")
-  handle:close()
-
-  if exit_status == 0 then
-    return true
-  else
-    return false
-  end
-end
-
-local function save_to_ssm(text, path, profile)
-
-
-  local overwrite = "--no-overwrite"
-
-  if check_parameter(path, profile) then
-    local answer vim.fn.input("Parameter already exists. Do you want to overwrite it? (y/n): ")
-    if answer == 'n' then
-      send_notification("Parameter not saved.", vim.log.levels.INFO)
-    else
-      overwrite = "--no-overwrite"
-    end
-    return
-  end
-
-
-  local cmd = string.format( 
-    "aws ssm put-parameter --name '%s' --value '%s' --type SecureString --profile %s %s > /dev/null 2>&1 ; echo $?",
-    path, text, profile, overwrite
-  )
-
-  local handle = io.popen(cmd)
-  local exit_status = handle:read("*n")
-  handle:close()
-
-  if exit_status == 0 then
-    send_notification("Parameter saved successfully!", vim.log.levels.INFO)
-  else
-    send_notification("Failed to save parameter. Exit status: " .. exit_status, vim.log.levels.ERROR)
-  end
-end
-
-function M.ssm()
-  local clipboard_content = vim.fn.getreg('*')
-
-  local text
-  if clipboard_content ~= '' then
-    local answer = vim.fn.input("Clipboard detected (y/n): ")
-    if answer == 'y' then
-      text = clipboard_content
-      send_notification("Clipboard content used as parameter text.", vim.log.levels.INFO)
-    else
-      text = vim.fn.input("Enter Text: ")
-    end
-  end
-
-  local path = vim.fn.input("Enter Path: ")
-  local profile = vim.fn.input("Enter Profile: ")
-
-  send_notification("\n", vim.log.levels.INFO)
-
-  save_to_ssm(text, path, profile)
 end
 
 return M
---
+++ lua/aws-ssm/ssm.lua
@@ -0,0 +1,94 @@
+-- ssm.lua
+local M = {}
+
+local use_fidget = false
+
+function M.setup(options)
+  if options and options.fidget then
+    use_fidget = true
+  end
+end
+
+local function send_notification(message, level)
+  if use_fidget then
+    local fidget = require('fidget')
+    if fidget and fidget.notify then
+      fidget.notify(message, level)
+    else
+      vim.notify(message, level)
+    end
+  else
+    vim.notify(message, level)
+  end
+end
+
+local function check_parameter(path, profile)
+  local cmd = string.format(
+    "aws ssm get-parameter --name '%s' --profile %s > /dev/null 2>&1 ; echo $?",
+    path, profile
+  )
+
+  local handle = io.popen(cmd)
+  local exit_status = handle:read("*n")
+  handle:close()
+
+  if exit_status == 0 then
+    return true
+  else
+    return false
+  end
+end
+
+local function save_to_ssm(text, path, profile)
+  local overwrite = "--no-overwrite"
+
+  if check_parameter(path, profile) then
+    local answer = vim.fn.input("Parameter already exists. Do you want to overwrite it? (y/n): ")
+    if answer == 'n' then
+      send_notification("Parameter not saved.", vim.log.levels.INFO)
+      return
+    else
+      overwrite = "--overwrite"
+    end
+  end
+
+  local cmd = string.format(
+    "aws ssm put-parameter --name '%s' --value '%s' --type SecureString --profile %s %s > /dev/null 2>&1 ; echo $?",
+    path, text, profile, overwrite
+  )
+
+  local handle = io.popen(cmd)
+  local exit_status = handle:read("*n")
+  handle:close()
+
+  if exit_status == 0 then
+    send_notification("Parameter saved successfully!", vim.log.levels.INFO)
+  else
+    send_notification("Failed to save parameter. Exit status: " .. exit_status, vim.log.levels.ERROR)
+  end
+end
+
+function M.ssm()
+  local clipboard_content = vim.fn.getreg('*')
+
+  local text
+  if clipboard_content ~= '' then
+    local answer = vim.fn.input("Clipboard detected (y/n): ")
+    if answer == 'y' then
+      text = clipboard_content
+      send_notification("Clipboard content used as parameter text.", vim.log.levels.INFO)
+    else
+      text = vim.fn.input("Enter Text: ")
+    end
+  end
+
+  local path = vim.fn.input("Enter Path: ")
+  local profile = vim.fn.input("Enter Profile: ")
+
+  send_notification("\n", vim.log.levels.INFO)
+
+  save_to_ssm(text, path, profile)
+end
+
+return M
+
